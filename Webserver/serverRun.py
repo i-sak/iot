3892b751ede7844a90568c09287afe35579f3493
i-sak <isaac7263@naver.com>
2020-09-06 19:23:58 +0900

    약간수정

--- Webserver/serverRun.py
+++ Webserver/serverRun.py
@@ -9,12 +9,12 @@ app = Flask(__name__)	# Flask object Assign to app
 def index() :
 	return render_template('index.html')
 
+# signin [log-in]
 @app.route("/signin", methods=['POST', 'GET'])
 def siginin() :
     if request.method == 'POST' :
         _id = request.form.get('_id')   # post로 보낼 경우, request.form.get으로 받고, get으로 보내는 경우 requst.args.get으로 받음
         _password = request.form.get('_password')
-        print(_id, _password)
         if _id == "1" and _password == "1" :
             return "login"
         else :
