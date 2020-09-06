0655b1a15b70b51e13219980192c6f18d5204a16
i-sak <isaac7263@naver.com>
2020-09-06 21:50:54 +0900

    iot 페이지

--- Webserver/serverRun.py
+++ Webserver/serverRun.py
@@ -18,7 +18,7 @@ def siginin() :
         if _id == "1" and _password == "1" :
             return "login"
         else :
-            return render_template('index.html')
+            return render_template('menu.html')
 
 host_addr = "0.0.0.0"
 port_num = "8080"
