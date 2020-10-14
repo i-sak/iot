3e1b6c5c35906e573dc2237a858608ad309e34ab
i-sak <isaac7263@naver.com>
2020-10-14 23:55:00 +0900

    Vo modify

--- Webserver/vo/tempVo.py
+++ Webserver/vo/tempVo.py
@@ -1,7 +1,8 @@
 class tempVo :
-    def __init__(self, t_time, t_temp, t_humi) :
-        self.t_no
+    def __init__(self, t_time, t_temp, t_humi, t_no) :
+        
         self.t_time = t_time
         self.t_temp = t_temp
         self.t_humi = t_humi
 
+        self.t_no = t_no
