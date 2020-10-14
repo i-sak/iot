cb67779336798ffc3de960a58e0b95bb443ec040
i-sak <isaac7263@naver.com>
2020-10-15 00:06:27 +0900

    list select update2

--- Webserver/vo/tempVo.py
+++ Webserver/vo/tempVo.py
@@ -1,8 +1,5 @@
 class tempVo :
-    def __init__(self, t_time, t_temp, t_humi, t_no) :
-        
+    def __init__(self, t_time, t_temp, t_humi) :
         self.t_time = t_time
         self.t_temp = t_temp
         self.t_humi = t_humi
-
-        self.t_no = t_no
